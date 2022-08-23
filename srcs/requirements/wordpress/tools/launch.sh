08e340d980c9d28dcbc3643f3b7de7ef7524ee2f
BERREDJEM DEL-VALLE Jorys <jberredj@student.42.fr>
2022-08-23 03:03:26 +0200

    Typo in Var

--- srcs/requirements/wordpress/tools/launch.sh
+++ srcs/requirements/wordpress/tools/launch.sh
@@ -8,7 +8,7 @@ if [ ! -f /var/www/html/wp-config.php ]; then
 	echo wp config create --dbhost=mariadb:3306 --dbname=${MARIADB_DB} --dbuser=${MARIADB_USR} --dbpass=${MARIADB_USRPASSWD} --dbcharset="utf8" --allow-root
 	wp config create --dbhost=mariadb:3306 --dbname=${MARIADB_DB} --dbuser=${MARIADB_USR} --dbpass=${MARIADB_USRPASSWD} --dbcharset="utf8" --allow-root
 	wp core install --url=${WP_SITE_URL} --title="${WP_SITE_TITLE}" --admin_user=${WP_ADM} --admin_password=${WP_ADM_PASSWD} --admin_email="$WP_ADM@42.fr" --allow-root
-	wp user create ${WP_USR} "$WP_USR"@student.42.fr --role=author --user_pass=${WP_USR_PASSWORD} --allow-root
+	wp user create ${WP_USR} "$WP_USR"@student.42.fr --role=author --user_pass=${WP_USR_PASSWD} --allow-root
 	chown -R www-data:www-data /var/www/html/
 fi
 
